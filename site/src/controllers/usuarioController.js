771bfeb945684d1f58062511cbeba89537ea1095
GabrielYukioMC <gabriel.coca@sptech.school>
2023-04-13 20:05:20 -0300

    api conectada

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -33,7 +33,7 @@ function entrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está indefinida!");
     } else {
-        
+
         usuarioModel.entrar(email, senha)
             .then(
                 function (resultado) {
@@ -62,9 +62,13 @@ function entrar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nome = req.body.nomeServer; 
+    var cnpj = req.body.cnpjServer;
+    var telefone = req.body.telefoneServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+
+
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -74,9 +78,9 @@ function cadastrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
-        
+
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome,cnpj ,telefone, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
